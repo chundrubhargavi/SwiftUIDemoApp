eccc87042ed327df2736e60ecc16f498662ae423
Bhargavi chundru <chodybhargavi@gmail.com>
2019-11-21 17:07:37 +0530

    Initial Commit

---
+++ LoginSwiftUITask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginSwiftUITask
+//
+//  Created by Amruta on 21/11/19.
+//  Copyright © 2019 Bhargavi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
